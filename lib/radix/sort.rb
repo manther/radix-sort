7758c596d8b2c341b47730a12a599b348db0c944
Manther <jason.manther.young@gmail.com>
2016-05-23 14:04:36 -0500

    Removed line.

--- lib/radix/sort.rb
+++ lib/radix/sort.rb
@@ -5,13 +5,12 @@ class Array
 
   def radix_sort
     input_array  = dup
-    buckets      = Array.new(10) { Array.new }
     (Math.log10(input_array.max.abs).floor + 1).times { |index|
+      buckets      = Array.new(10) { Array.new }
       input_array.each { |val|
         buckets[val % (10 ** (index + 1)) / (10 ** (index))] << val
       }
       input_array  = buckets.flatten
-      buckets      = Array.new(10) { Array.new }
     }
     input_array
   end
