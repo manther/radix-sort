1232dc15187c90da58edfb201c4558c7efa3c814
Manther <jason.manther.young@gmail.com>
2016-05-23 11:58:45 -0500

    Radix-Sort

--- lib/radix/sort.rb
+++ lib/radix/sort.rb
@@ -1,7 +1,18 @@
-require "radix/sort/version"
+class Array
+  def radix_sort!
+    replace radix_sort
+  end
 
-module Radix
-  module Sort
-    # Your code goes here...
+  def radix_sort
+    input_array  = dup
+    buckets      = Array.new(10) { Array.new }
+    (Math.log10(input_array.max.abs).floor + 1).times { |index|
+      input_array.each { |val|
+        buckets[val % (10 ** (index + 1)) / (10 ** (index))] << val
+      }
+      input_array  = buckets.flatten
+      buckets      = Array.new(10) { Array.new }
+    }
+    input_array
   end
 end
